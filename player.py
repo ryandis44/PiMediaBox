d9241d7a649cec504b116de249c915474e26d355
ryandis44 <ryandisanti5810@gmail.com>
2024-08-01 02:40:55 -0500

    Update player.py

--- player.py
+++ player.py
@@ -11,8 +11,10 @@ videos = []
 def getVideos():
     global videos
     videos = []
-    for file in os.listdir(directory):
-        if file.lower().endswith('.mp4'):
+    __dir = os.listdir(directory)
+    __dir.sort()
+    for file in __dir:
+        if file.lower().endswith('.mkv'):
             videos.append(os.path.join(directory, file))
 
 
@@ -20,13 +22,43 @@ def playVideos():
     global videos
     if len(videos) == 0:
         getVideos()
-        time.sleep(5)
+        # time.sleep(5)
         return
-    random.shuffle(videos)
+    
+    # Start at a random episode and continue from there
+    __num = random.randint(0, len(videos)-1)
+    print(
+        f"__num: {__num}\n"
+        f"videos: {len(videos)}\n"
+    )
+    
+
+    print(
+        f"{videos[__num - 1]}\n"
+        f"{videos[__num]}\n"
+        f"{videos[__num + 1]}\n"
+    )
+    
     for video in videos:
-        playProcess = Popen(['omxplayer', '--no-osd', '--aspect-mode', 'fill', video])
-        playProcess.wait()
+        print(video)
+    
+    # Play videos in order from random starting episode
+    while True:
+        
+        # Set current video to play. If it fails,
+        # we have reached the end of the queue.
+        # Restart from the beginning and requeue.
+        try: video = videos[__num]
+        except:
+            __num = 0
+            continue
+        
+        try:
+            playProcess = Popen(['cvlc', video, '--sub-track=1000', '--no-video-title-show'])
+            playProcess.wait()
+        except: pass
+        __num += 1 # Go to next episode after completion or failure
 
 
-while (True):
-    playVideos()
+# Run the player continuously
+while (True): playVideos()
