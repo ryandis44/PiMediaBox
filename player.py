5fe2e32abbfdeefbca4d470f6461d3f2a665881b
ryandis44 <ryandisanti5810@gmail.com>
2024-08-13 23:12:25 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,26 +1,21 @@
 import os
 import random
+import vlc
 import time
-from subprocess import PIPE, Popen, STDOUT
 
 directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos')
 
 videos = []
+__dir = os.listdir(directory)
+__dir.sort()
+for file in __dir:
+    if file.lower().endswith('.mkv'):
+        videos.append(os.path.join(directory, file))
 
-
-def getVideos():
-    global videos
-    videos = []
-    __dir = os.listdir(directory)
-    __dir.sort()
-    for file in __dir:
-        if file.lower().endswith('.mkv'):
-            videos.append(os.path.join(directory, file))
-
+instance = vlc.Instance('--no-video-title-show', '--sub-track=1000')  # No subtitles, no file name display
+player = instance.media_player_new()
 
 def playVideos():
-    global videos
-    if len(videos) == 0: getVideos()
     
     # Start at a random episode and continue from there
     __num = random.randint(0, len(videos)-1)
@@ -37,11 +32,22 @@ def playVideos():
             continue
         
         try:
-            playProcess = Popen(['cvlc', video, '--sub-track=1000', '--no-video-title-show'])
-            playProcess.wait()
+            
+            media = instance.media_new(video)
+            player.set_media(media)
+            player.play()
+            while player.get_state() != vlc.State.Ended:
+                time.sleep(1)
+            
+            # subprocess.check_call(['cvlc', video, '--sub-track=1000', '--no-video-title-show'])
+
+            # playProcess = Popen(['cvlc', video, '--sub-track=1000', '--no-video-title-show'])
+            # playProcess.wait()
         except: pass
         __num += 1 # Go to next episode after completion or failure
 
 
 # Run the player continuously
-while (True): playVideos()
+while True:
+    try: playVideos()
+    except: pass
